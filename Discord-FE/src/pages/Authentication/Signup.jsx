a664f7506133a9bfb1e992a48413dc42f0a8cb1f
PG47 <86549123+PG47@users.noreply.github.com>
2025-03-12 15:16:00 +0700

    fix back Sign up UI

--- Discord-FE/src/pages/Authentication/Signup.jsx
+++ Discord-FE/src/pages/Authentication/Signup.jsx
@@ -7,8 +7,10 @@ import './Authentication.css';
 const Signup = () => {
   const { isDarkMode } = useTheme();
 
-  const [username, setUsernam] = useState('');
+  const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const [errorMessage, setErrorMessage] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
@@ -27,8 +29,8 @@ const Signup = () => {
   const handleSignup = async (e) => {
     e.preventDefault();
 
-    if (!username) {
-      setErrorMessage('Uername không được để trống');
+    if (!email || !validateEmail(email)) {
+      setErrorMessage('Email không hợp lệ');
       return;
     }
 
@@ -37,11 +39,17 @@ const Signup = () => {
       return;
     }
 
+    if (!password) {
+      setErrorMessage('Mật khẩu không được để trống');
+      return;
+    }
+
+
     try {
       const response = await fetch('http://localhost:5000/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, phone }),
+        body: JSON.stringify({ email, phone, password }),
       });
 
       if (!response.ok) {
@@ -83,8 +91,29 @@ const Signup = () => {
           Đăng ký
         </h2>
           <form onSubmit={handleSignup} className='flex flex-col gap-4 mt-7'>
-            <input type="username" placeholder='Username' className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl' value={username} onChange={(e) => setUsernam(e.target.value)} />
+            <input type="email" placeholder='Email' className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl' value={email} onChange={(e) => setEmail(e.target.value)} />
             <input type="tel" placeholder='Số điện thoại' className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl' value={phone} onChange={(e) => setPhone(e.target.value)} />
+            <div className="relative w-full">
+              <input 
+                type={showPassword ? "text" : "password"} 
+                placeholder='Mật khẩu' 
+                className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button 
+                type="button" 
+                className={`absolute right-4 top-1/2 transform -translate-y-1/2 bg-transparent p-1 border-none focus:outline-none ${isDarkMode 
+                  ? "text-white" 
+                  : "text-gray-500"}
+                `}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <FaEye /> : <FaEyeSlash />}
+                </button>
+            </div>
+
+            {/* Submit form button to go next step*/}
             <button type='submit' className={`btn_dark_rounded my-5 w-full !rounded-md ${isDarkMode 
               ? "from-red-400 via-white-500 to-yellow-400" 
               : "from-purple-700 via-blue-500 to-green-400"}
@@ -93,11 +122,11 @@ const Signup = () => {
             </button>
             
             <p className={`flex items-center justify-center font-bold gap-1${isDarkMode 
-              ? "text-[#616A6F]" 
+              ? "text-white" 
               : "text-black"}
             `}>
               Đã có tài khoản? &nbsp;
-              <Link to="/login" className={`text-secondary underline cursor-pointer ${isDarkMode 
+              <Link to="/login" className={`underline ${isDarkMode 
                 ? "text-white" 
                 : "text-blue-800"}
               `}>
